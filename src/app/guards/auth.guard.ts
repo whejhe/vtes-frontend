9ad90c3b8da599dc2f28a0ba23dfc51a22017de1
Carlos Fernandez <dominguezalacid@gmail.com>
2024-04-08 07:18:11 +0200

    actualizando guardianes

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,1 +1,16 @@
 //front/src/app/guards/auth.guard.ts
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.getToken()) {
+    return true;
+  } else {
+    router.navigate(['/auth']);
+    return false;
+  }
+};
