aa2bed614951bcebf4c48b42d9e075884365fbe6
Carlos Fernandez <dominguezalacid@gmail.com>
2024-04-10 07:29:27 +0200

    actualizar

--- src/app/pages/main/list-cards/biblioteca/biblioteca.component.ts
+++ src/app/pages/main/list-cards/biblioteca/biblioteca.component.ts
@@ -2,7 +2,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { AsyncPipe, CommonModule, NgOptimizedImage } from '@angular/common';
-import { Card, Clan, Discipline, Title, Traits, Type } from '../../../../models/vtes.model';
+import {
+  Card,
+  Clan,
+  Discipline,
+  Title,
+  Traits,
+  Type,
+} from '../../../../models/vtes.model';
 import { JsonServiceService } from '../../../../services/json-service.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DetailsCardLibraryComponent } from '../../../../components/details-card-library/details-card-library.component';
@@ -12,40 +19,58 @@ import { FilterPipe } from '../../../../pipes/filter.pipe';
 import { IconService } from '../../../../services/icon.service';
 import { __values } from 'tslib';
 import { FilterMultiPipe } from '../../../../pipes/filter-multi.pipe';
+import { User } from '../../../../models/user.model';
+import { LocalStorageService } from '../../../../services/local-storage.service';
 
 @Component({
-    selector: 'app-biblioteca',
-    standalone: true,
-    templateUrl: './biblioteca.component.html',
-    styleUrl: './biblioteca.component.scss',
-    imports: [
-      AsyncPipe,
-      CommonModule,
-      FormsModule,
-      FilterPipe,
-      FilterMultiPipe
-    ]
-  })
-  export class BibliotecaComponent implements OnInit {
+  selector: 'app-biblioteca',
+  standalone: true,
+  templateUrl: './biblioteca.component.html',
+  styleUrl: './biblioteca.component.scss',
+  imports: [AsyncPipe, CommonModule, FormsModule, FilterPipe, FilterMultiPipe],
+})
+export class BibliotecaComponent implements OnInit {
+  user: User | null = null;
 
-    constructor(
-      private jsonSvc: JsonServiceService,
-      public dialog: MatDialog,
-      public iconSvc : IconService
-    ) {}
+  constructor(
+    private jsonSvc: JsonServiceService,
+    public dialog: MatDialog,
+    public iconSvc: IconService,
+    public localtorageSvc: LocalStorageService
+  ) {}
+
+  // Guardar datos del usuario
+  setItem(key: string, value: string): void {
+    this.localtorageSvc.setItem('user', JSON.stringify(this.user));
+  }
+
+  // Obtener datos del usuario
+  getItem(key: string): string | null {
+    return this.localtorageSvc.getItem(key);
+  }
+
+  // Eliminar datos del usuario
+  removeItem(key: string): void {
+    this.localtorageSvc.removeItem(key);
+  }
+
+  // Limpiar local storage
+  clear(): void {
+    this.localtorageSvc.clear();
+  }
 
   public cards!: Card[];
   public filter!: string;
-  public searchName:string = '';
+  public searchName: string = '';
   public searchSect: string = '';
 
   public url: string = '';
 
-  imageStyles!: { width: string; height: string; };
+  imageStyles!: { width: string; height: string };
 
   public disciplineImages = this.iconSvc.disciplineImages;
 
-  public disciplines = Object.values(Discipline)
+  public disciplines = Object.values(Discipline);
   public disciplineSelected: { [key: string]: boolean } = {};
   public selectedDisciplines: Discipline[] = [];
 
@@ -54,7 +79,9 @@ import { FilterMultiPipe } from '../../../../pipes/filter-multi.pipe';
   public titlesSelected: { [key in Title]?: boolean } = {};
   public selectedTitles: Title[] = [];
 
-  public types = Object.values(Type).filter(type => type !== 'Vampire' && type !== 'Imbued');
+  public types = Object.values(Type).filter(
+    (type) => type !== 'Vampire' && type !== 'Imbued'
+  );
   public typeSelected: { [key: string]: boolean } = {};
   public selectedTypes: Type[] = [];
 
@@ -76,42 +103,42 @@ import { FilterMultiPipe } from '../../../../pipes/filter-multi.pipe';
   public poolCostSelected: { [key: string]: boolean } = {};
   public selectedPoolCosts: string[] = [];
 
-
-  setUrlImage(url: string):void {
+  setUrlImage(url: string): void {
     this.url = url;
   }
 
-  clearUrlImage():void{
+  clearUrlImage(): void {
     this.url = '';
   }
 
-
-  openModal(card:Card):void{
+  openModal(card: Card): void {
     this.dialog.open(DetailsCardLibraryComponent, {
-      data: {card}
+      data: { card },
     });
   }
 
   //URL IMAGENES DISCIPLINAS
   getDisciplineUrl(disciplineName: string): string {
-    const discipline = this.disciplineImages.find(item => item.name === disciplineName);
+    const discipline = this.disciplineImages.find(
+      (item) => item.name === disciplineName
+    );
     return discipline ? discipline.url : '';
   }
 
   //URL IMAGENES COSTES PERSONAJES
-  getCapacityCostImage(capacityCost: number | undefined){
+  getCapacityCostImage(capacityCost: number | undefined) {
     this.iconSvc.getCapacityCostImage(capacityCost);
     return this.iconSvc.getCapacityCostImage(capacityCost);
   }
 
   //URL IMAGENES COSTES BOOD
-  getBloodCostImage(bloodCost: string | undefined){
+  getBloodCostImage(bloodCost: string | undefined) {
     this.iconSvc.getBloodCostImage(bloodCost);
     return this.iconSvc.getBloodCostImage(bloodCost);
   }
 
   //URL IMAGENES COSTES POOL
-  getPoolCostImage(poolCost: string | undefined){
+  getPoolCostImage(poolCost: string | undefined) {
     this.iconSvc.getPoolCostImage(poolCost);
     return this.iconSvc.getPoolCostImage(poolCost);
   }
@@ -133,26 +160,27 @@ import { FilterMultiPipe } from '../../../../pipes/filter-multi.pipe';
     this.onDisciplinesChange();
   }
 
-
   //SELECCIONAR DISCIPLINAS
   onDisciplinesChange(): void {
     this.selectedDisciplines = Object.keys(this.disciplineSelected)
-      .filter(discipline => this.disciplineSelected[discipline as Discipline])
-      .map(discipline => discipline as Discipline);
+      .filter((discipline) => this.disciplineSelected[discipline as Discipline])
+      .map((discipline) => discipline as Discipline);
   }
-
 
   //FILTRO PARA BIBLIOTECA
   onTypeChange(newValue: string): void {
     this.selectedTypes = Object.keys(this.typeSelected)
-      .filter(type => this.typeSelected[type as Type] && type !== 'Vampire' || type !== 'Imbued')
-      .map(type => type as Type)
-    }
+      .filter(
+        (type) =>
+          (this.typeSelected[type as Type] && type !== 'Vampire') ||
+          type !== 'Imbued'
+      )
+      .map((type) => type as Type);
+  }
 
   resetFilterType(): void {
     this.selectedTypes = [];
   }
-
 
   //FILTROS DE CLAN
   onSearchClanChange(newValue: string): void {
@@ -164,34 +192,32 @@ import { FilterMultiPipe } from '../../../../pipes/filter-multi.pipe';
     this.searchClan = '';
   }
 
-
   //FILTROS POR CARACTERISTICAS
   onTraitsChange(): void {
     this.selectedTraits = Object.keys(this.traitsSelected)
-      .filter(trait => this.traitsSelected[trait as Traits])
-      .map(trait => trait as Traits);
+      .filter((trait) => this.traitsSelected[trait as Traits])
+      .map((trait) => trait as Traits);
   }
 
   //FILTROS POR TITULOS
   onTitlesChange(): void {
     this.selectedTitles = Object.keys(this.titlesSelected)
-      .filter(title => this.titlesSelected[title as Title])
-      .map(title => title as Title);
+      .filter((title) => this.titlesSelected[title as Title])
+      .map((title) => title as Title);
   }
-
 
   //FILTROS POR COSTES
   onBloodCostsChange(): void {
     this.selectedBloodCosts = Object.keys(this.bloodCostSelected)
-      .filter(bloodCost => this.bloodCostSelected[bloodCost])
-      .map(bloodCost => bloodCost);
+      .filter((bloodCost) => this.bloodCostSelected[bloodCost])
+      .map((bloodCost) => bloodCost);
   }
 
   //FILTROS POR COSTES
   onPoolCostsChange(): void {
     this.selectedPoolCosts = Object.keys(this.poolCostSelected)
-      .filter(poolCost => this.poolCostSelected[poolCost])
-      .map(poolCost => poolCost);
+      .filter((poolCost) => this.poolCostSelected[poolCost])
+      .map((poolCost) => poolCost);
   }
 
   ngOnInit(): void {
