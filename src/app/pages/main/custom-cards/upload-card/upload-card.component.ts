1075d06037c879be64cc82adc32578c7a42c2df3
Carlos Fernandez <dominguezalacid@gmail.com>
2024-04-12 20:21:16 +0200

    actualizar

--- src/app/pages/main/custom-cards/upload-card/upload-card.component.ts
+++ src/app/pages/main/custom-cards/upload-card/upload-card.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../../../models/user.model';
+import { AuthService } from '../../../../services/auth.service';
 
 @Component({
   selector: 'app-upload-card',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './upload-card.component.html',
   styleUrl: './upload-card.component.scss'
 })
-export class UploadCardComponent {
+export class UploadCardComponent implements OnInit {
 
+  constructor(
+    public authSvc: AuthService) {}
+
+  user:User[] = []
+
+  getCurrentUser(): User | null {
+    return this.authSvc.getCurrentUser();
+  }
+
+
+  ngOnInit(): void {
+    this.getCurrentUser();
+  }
 }
