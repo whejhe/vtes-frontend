dab0f987ea2d5dc397f70f17932addb53d40d884
Carlos Fernandez <dominguezalacid@gmail.com>
2024-04-13 07:13:31 +0200

    actualizar

--- src/app/pages/main/custom-cards/gallery/gallery.component.ts
+++ src/app/pages/main/custom-cards/gallery/gallery.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SearchImputComponent } from "../../../../components/search-imput/search-imput.component";
 import { FormsModule } from '@angular/forms';
 import { FilterMultiPipe } from '../../../../pipes/filter-multi.pipe';
+import { AuthService } from '../../../../services/auth.service';
+import { User } from '../../../../models/user.model';
 
 @Component({
     selector: 'app-gallery',
@@ -14,11 +16,29 @@ import { FilterMultiPipe } from '../../../../pipes/filter-multi.pipe';
         FilterMultiPipe
     ]
 })
-export class GalleryComponent {
+export class GalleryComponent implements OnInit {
 
     constructor(
-        
+        public authSvc: AuthService
     ) {}
 
+    user:User[] = [];
+
+    getUsers() {
+      this.authSvc.getUsers().subscribe(
+          (users: User[]) => {
+              this.user = users;
+              console.log('Users: ', this.user);
+          },
+          (error) => {
+              console.log('Error al obtener los usuarios: ', error);
+          });
+  }
+
+
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
 
 }
