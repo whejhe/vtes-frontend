3b468b58d5e24d168d21bfad163b4eca3a9c7d67
Carlos Fernandez <dominguezalacid@gmail.com>
2024-05-19 21:33:41 +0200

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,5 @@
-import { initializeApp } from "firebase/app";
 
 export const environment = {
   production: true,
   apiUrl: 'https://vtesapp.duckdns.org',
-  firebaseConfig : {
-    apiKey: "AIzaSyAQwn0Ez3iUke7plRTZhhCVT1KeCJAbHoY",
-    authDomain: "vtesimage.firebaseapp.com",
-    projectId: "vtesimage",
-    storageBucket: "vtesimage.appspot.com",
-    messagingSenderId: "895034302185",
-    appId: "1:895034302185:web:77221e3dafb8683a7b1a78"
-  }
 };
-
-
-// Initialize Firebase
-const firebaseConfig = environment.firebaseConfig;
-export const app = initializeApp(firebaseConfig);
